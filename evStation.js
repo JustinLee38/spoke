72fa5e26eb1a3aa50c711663efe224eb9470aeba
justin <glee99@MGC12DQ2F8MD6N.fbpld77.ford.com>
2022-06-14 22:21:13 -0700

    ev-charging

---
+++ evStation.js
@@ -0,0 +1,12 @@
+const userAction = async () => {
+  const response = await fetch("https://developer.nrel.gov/api/alt-fuel-stations/v1.json?fuel_type=ELEC&status=E&limit=2&api_key=3lyPvLFhq5yVcoOuKf8hzlm5Hl2rNeAkaX65Hw0c", {
+    method: "GET",
+    body: myBody, // string or object
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+  const myJson = await response.json(); //extract JSON from the http response
+  // do something with myJson
+  console.log(myJson)
+};
